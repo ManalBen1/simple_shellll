0b30e3fe546ac3f9dc332bca2fc36c145457ea4a
ManalBen1 <manaltini@gmail.com>
2023-08-24 13:41:48 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
 {
 printf("Hey, enter the command here: ");
-return(0);
+return (0);
 }
